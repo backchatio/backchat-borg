f9eecfa5b4b373cb49d9ab48d7c3d6690c359aa0
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-12-10 08:55:25 +0100

    separate config parsing from config models

--- src/main/scala/backchat/borg/cadence/Alerter.scala
+++ src/main/scala/backchat/borg/cadence/Alerter.scala
@@ -3,19 +3,17 @@ package borg
 package cadence
 
 import com.ning.http.client.AsyncHttpClient
-import java.util.concurrent.TimeUnit
 import net.liftweb.json._
 import mojolly._
 import config.{ ConfigurationContext, MojollyConfig }
 import metrics.MetricsConfig
-import akka.actor.Scheduler
 import akka.util.Bootable
 import Alerter._
 
 /**
  * Polls Graphite and logs when a metric passes a threshold.
  */
-class Alerter(config: AlerterConfig) extends Bootable with Logging {
+class Alerter(config: AlerterContext) extends Bootable with Logging {
   lazy val httpClient = new AsyncHttpClient
 
   lazy val alertLogger = Logger("ALERTS")
@@ -23,7 +21,7 @@ class Alerter(config: AlerterConfig) extends Bootable with Logging {
   override def onLoad() {
     super.onLoad()
     logger debug ("polling %s every %d seconds: " format (config.url, config.period))
-    Scheduler schedule (() ⇒ run, 0, config.period, TimeUnit.SECONDS)
+    Timer(config.period.seconds)(run)
   }
 
   override def onUnload() {
@@ -71,20 +69,23 @@ class Alerter(config: AlerterConfig) extends Bootable with Logging {
 object Alerter {
   class AlerterConfig(key: String = "application") extends MojollyConfig(ConfigurationContext(key)) with MetricsConfig {
     val applicationName = "Alerter"
-    val alerts = config.getSection("mojolly.borg.cadence.alerts") map { section ⇒
-      section.keys.map(_.split("\\.", 2)).map(_.head) map { k ⇒
-        Alert(
-          target = section.getString(k + ".graphitekeypattern").get,
-          warn = section.getDouble(k + ".error").get,
-          error = section.getDouble(k + ".warn").get)
-      }
-    } getOrElse Nil
-    val url = "http://%s" format (reporting map (_.host) getOrElse "graphite")
-    val period = config.getSection("mojolly.reporting") flatMap { sect ⇒
-      sect.getInt("pollInterval")
-    } getOrElse 60
+    val alerter = AlerterContext(
+      "http://%s" format (reporting map (_.host) getOrElse "graphite"),
+      config.getSection("mojolly.reporting") flatMap { sect ⇒
+        sect.getInt("pollInterval")
+      },
+      config.getSection("mojolly.borg.cadence.alerts") map { section ⇒
+        section.keys.map(_.split("\\.", 2)).map(_.head) map { k ⇒
+          Alert(
+            section.getString(k + ".graphitekeypattern").get,
+            section.getDouble(k + ".error").get,
+            section.getDouble(k + ".warn").get)
+        }
+      } getOrElse Nil
+    )
   }
 
+  case class AlerterContext(url: String, period: Int, alerts: Seq[Alert])
   case class Alert(target: String, warn: Double, error: Double)
 
   case class GraphiteResponse(metrics: List[Metric])
