0d29306b439d72babb70f11212640afbb6f31d6d
Ivan Porto Carrero <ivan@flanders.co.nz>
2011-12-12 19:03:11 +0100

    Adds specs for sending a tell message from a reliable and an unreliable client

--- src/main/scala/backchat/borg/telepathy/Server.scala
+++ src/main/scala/backchat/borg/telepathy/Server.scala
@@ -3,11 +3,15 @@ package borg
 package telepathy
 
 import Messages._
-import akka.actor.ActorRef
+import akka.actor._
+import Actor._
 import akka.zeromq._
 import akka.dispatch.{ Future }
 
 case class ServerConfig(listenOn: TelepathAddress, socket: Option[ActorRef] = None)
+object ClientSession {
+  def apply(request: ZMQMessage): ClientSession = ClientSession(request.frames.head.payload)
+}
 case class ClientSession(clientId: Seq[Byte])
 object Server {
   type Respond = PartialFunction[BorgMessageWrapper, Future[Any]]
@@ -32,16 +36,30 @@ class Server(config: ServerConfig) extends Telepath {
       logger info "Server %s is ready".format(self.id)
     }
     case request: ZMQMessage if responseFor.isDefinedAt(deserialize(request)) ⇒
-      (deserialize _ andThen responseFor)(request) onResult {
+      (deserialize _ andThen hugClientIfInNeed(request) andThen responseFor)(request) onResult {
         case response: BorgMessageWrapper ⇒ socket ! mkReply(request, response)
-        case 'addToClients                ⇒ activeClients :+= ClientSession(request.frames.head.payload)
+        case 'addToClients                ⇒ addToClients(ClientSession(request))
         case _                            ⇒ // ignore the other ones 
       }
+  }
+
+  protected def addToClients(session: ClientSession) = {
+    if (!(activeClients contains session))
+      activeClients :+= session
+  }
+
+  def hugClientIfInNeed(request: ZMQMessage): PartialFunction[BorgMessageWrapper, BorgMessageWrapper] = {
+    case m: HiveRequest ⇒ {
+      if (activeClients contains ClientSession(request)) socket ! Hug(m.ccid)
+      m
+    }
+    case m ⇒ m
   }
 
   protected val responseFor: Respond = {
     case Ping           ⇒ Future { Pong }
     case a @ CanHazHugz ⇒ Future { 'addToClients }
+    case m: Tell        ⇒ Future { registry.actorsFor(m.target).headOption foreach { _ ! m.payload } }
   }
 
   val serializer = new BorgZMQMessageSerializer
--- src/test/scala/backchat/borg/telepathy/tests/ServerSpec.scala
+++ src/test/scala/backchat/borg/telepathy/tests/ServerSpec.scala
@@ -12,6 +12,9 @@ import Messages._
 import mojolly.io.FreePort
 import akka.zeromq.{Bind, Send, Frame, ZMQMessage}
 import scalaz.Scalaz._
+import org.specs2.execute.Result
+import net.liftweb.json._
+import java.util.concurrent.{CountDownLatch, TimeUnit}
 
 class ServerSpec extends AkkaSpecification { def is =
   "A Server should" ^
@@ -19,8 +22,8 @@ class ServerSpec extends AkkaSpecification { def is =
     "tracks active pubsub client sessions" ! pending ^ // on first subscription
     "tracks active reliable client sessions" ! specify.tracksReliableClientSessions ^ bt ^ // CanHazHugz
     "when receiving a tell message" ^
-      controlMessages(null) ^ bt  ^
-    "when receiving an ask message" ^
+      controlMessages(tellSpec) ^ bt ^ end
+    /*"when receiving an ask message" ^
       "reply with the response" ! pending ^
       controlMessages(null) ^ bt  ^
     "when receiving a shout message" ^
@@ -32,32 +35,95 @@ class ServerSpec extends AkkaSpecification { def is =
     "when receiving a deafen message" ^
       "remove the listener from the active subscriptions" ! pending ^
       controlMessages(null) ^
-  end
+  end*/
   
   def controlMessages(req: RequestContext) = {
-    "route to the correct handler" ! pending ^
-    "do nothing for reliable false" ! pending ^
-    "send hug for reliable true" ! pending
+    "route to the correct handler" ! req.routesToCorrectHandler ^
+    "do nothing for reliable false" ! req.doesNothingForUnreliable ^
+    "send hug for reliable true" ! req.sendsHugForReliableClient
   }
 
-  trait RequestContext
+  trait ZMQServerContext extends TestKit {
 
-  def specify = new ServerContext
-  
-  class ServerContext extends RequestContext with TestKit {
-    
     val clientId = {
       val newId = Array.empty[Byte]
       Random.nextBytes(newId)
       newId
     }
-    
+
     val port = FreePort.randomFreePort()
     val address = TelepathAddress("127.0.0.1", port)
     val addressUri = address.address
     lazy val serverConfig = ServerConfig(address, testActor.some)
-    
-    
+    def mkMessage(msg: BorgMessageWrapper) = {
+      ZMQMessage(Seq(Frame(clientId), Frame(msg.toBytes)))
+    }
+  }
+  
+  trait RequestContext extends ZMQServerContext {
+    def routesToCorrectHandler: Result
+    def doesNothingForUnreliable: Result
+    def sendsHugForReliableClient: Result
+  }
+
+  def specify = new ServerContext
+  def tellSpec = new TellContext
+  
+  class TellContext extends RequestContext {
+    def routesToCorrectHandler = {
+      val l = TestLatch()
+      val target = "the-target"
+      actorOf(new Actor {
+        self.id = target
+        protected def receive = {
+          case ApplicationEvent('pingping, JNothing) => l.countDown
+        }
+      }).start()
+
+      val server = TestActorRef(new Server(serverConfig)).start()
+      server ! mkMessage(CanHazHugz)
+      server ! mkMessage(Tell(target, ApplicationEvent('pingping)))
+      l.await(2 seconds) must beTrue
+    }
+
+    def doesNothingForUnreliable = {
+      val socketLatch = new CountDownLatch(2)
+      val target = "the-target-2"
+      val msg = Tell(target, ApplicationEvent('pingping))
+      val expected = Hug(msg.ccid)
+      val socket = actorOf(new Actor {
+        def receive = {
+          case Bind(`addressUri`) => socketLatch.countDown
+          case `expected` => socketLatch.countDown
+        }  
+      }).start()
+      val server = TestActorRef(new Server(serverConfig.copy(socket = socket.some))).start()
+
+      server ! mkMessage(msg)
+      socketLatch.await(2, TimeUnit.SECONDS) must beFalse
+    }
+
+    def sendsHugForReliableClient = {
+      val socketLatch = new CountDownLatch(2)
+      val target = "the-target-3"
+      val msg = Tell(target, ApplicationEvent('pingping))
+      val expected = Hug(msg.ccid)
+      val socket = actorOf(new Actor {
+        def receive = {
+          case Bind(`addressUri`) => socketLatch.countDown
+          case `expected` => socketLatch.countDown
+        }  
+      }).start()
+      val server = TestActorRef(new Server(serverConfig.copy(socket = socket.some))).start()
+      server ! mkMessage(CanHazHugz)
+      sleep -> 100.millis
+      server ! mkMessage(msg)
+      socketLatch.await(2, TimeUnit.SECONDS) must beTrue
+    }
+  }
+  
+  class ServerContext extends ZMQServerContext {
+
     def respondsWithPong = {
       val latch = TestLatch(2)
       val expected = Send(Seq(Frame(clientId), Frame(Pong.toBytes)))
@@ -78,8 +144,6 @@ class ServerSpec extends AkkaSpecification { def is =
       server.underlyingActor.activeClients must be_==(Vector(ClientSession(clientId))).eventually
     }
 
-    def mkMessage(msg: BorgMessageWrapper) = {
-      ZMQMessage(Seq(Frame(clientId), Frame(msg.toBytes)))
-    }
+
   }
 }
